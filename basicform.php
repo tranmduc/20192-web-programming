0b860dd09a0dbea7c20d5384619f080c706269fb
hirotatsuya <galileoducgalile@gmail.com>
2020-03-10 10:40:42 +0700

    no message

---
+++ basicform.php
@@ -0,0 +1,14 @@
+<html>
+ <head><title> A Simple Form </title> </head>
+ <body>
+     <form ACTION="http://fit.hust.edu.vn/~tarngntt/courses/wp" METHOD="POST">
+         Click submit to start our initial PHP program.
+         <br>
+         <input type="text" name="Name">
+         <br>
+         <input type="submit" value="Click to Submit">
+         <input type="reset" value="Erase and Restart">
+     </form>
+ </body>
+</html>
+
---
+++ example.php
@@ -0,0 +1,7 @@
+<form action="/Univ/20192-web-programming/email.php" method="POST">
+<p>
+    Enter email address: <input type="text" size="16" maxlength="20" name="email">
+    Contact is preferred: <input type="radio" name="contact" value="Yes">
+    <input type="submit" value="Send"
+></p>
+</form>
