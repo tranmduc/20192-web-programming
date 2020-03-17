7c6121713e533b9a6980bea27a893461bfe2160e
hirotatsuya <galileoducgalile@gmail.com>
2020-03-17 08:54:57 +0700

    no message

---
+++ Lab2/Form4Radio.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Receiving input</title>
+    </head>
+    <body>
+        <h1>Thank you: got your input</h1>
+        <?php
+          $email = $_POST['email'];
+          $contact = $_POST['contact'];
+          print ("<br>Your email is $email");
+          print ("<br>Contact preference is  $contact");
+        ?>
+    </body>
+</html>
---
+++ Lab2/ex29.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <title>Form receive</title>
+</head>
+<body>
+    <p>Thank you, got your input</p>
+    <?php 
+        $userName = $_POST['name'];
+        $class = $_POST['class'];
+        $university = $_POST['univ'];
+        $gender = $_POST['gender'];
+        $hobbie = $_POST['hobbie'];
+
+        print("<p>Hello $userName, class $class, univerity $university</p>");
+        print("<p>You are a $gender</p>");
+        print("<p>You like");
+        for($x=0; $x<count($hobbie); $x++){
+            print(" $hobbie[$x],");
+        }
+        print("</p>");
+    ?>
+</body>
+</html>
