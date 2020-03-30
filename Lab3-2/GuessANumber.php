459d84f45f7d7de94fa84a6bf1b38c7c6a36ae35
hirotatsuya <galileoducgalile@gmail.com>
2020-03-31 00:07:16 +0700

    no message

--- Lab3-2/GuessANumber.php
+++ Lab3-2/GuessANumber.php
@@ -1,78 +1,75 @@
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
+<html>
+  <head>
+    <title>Guess a number</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Guess a Number</title>
-</head>
-
-<body>
+  </head>
+  <body>
+    <h1>Guess a number</h1>
     <?php
-    if (array_key_exists('randomResult', $_GET)) {
-        $randomResult = $_GET['randomResult'];
-    } else {
-        srand((float) microtime() * 10000000);
-        $randomResult = rand(1, 100);
-    }
-
-    if (array_key_exists('guess', $_GET)) {
-        $count = $_GET['count'];
-        $guess = $_GET['guess'];
-        if ($randomResult > $guess) {
-            print("<p>Wrong, please try a higher number. You have guess $count</p>");
-        } else if ($randomResult < $guess) {
-            print("<p>Wrong, please try a smaller number. You have guess $count</p>");
-        } else {
-            print("<p>CONGRATULATION, answer is $randomResult. You have guess $count</p>");
-        }
-        $count++;
-    } else {
-        $count = 1;
+      if (array_key_exists('result', $_POST)) {
+        $result = $_POST['result'];
+      } else {
+        srand ((double) microtime() * 10000000);
+        $result = rand(0, 100);
+      }
+      if (array_key_exists('guess', $_POST)) {
+        $guess = $_POST['guess'];
+      } else {
         $guess = '';
-    }
+      }
+      if (array_key_exists('count', $_POST)) {
+        $count = $_POST['count'] + 1;
+      } else {
+        $count = 0;
+      }
     ?>
-
-
-    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="get">
-        <input type="text" name="randomResult" value="<?php echo $randomResult ?>" style="display: none;">
-        <input type="text" name="count" value="<?php echo $count ?>" style="display: none;">
-        <table>
-            <tr>
-                <td>Your guess:</td>
-                <td>
-                    <input type="text" name="guess" value="<?php echo $guess; ?>">
-                </td>
-            </tr>
-            <?php if ($guess !== '') : ?>
-                <tr>
-                    <td>
-                        <?php
-                        if (is_numeric($guess)) {
-                            if ($guess == $result) {
-                                echo "You are correct!";
-                            } elseif ($guess < $result) {
-                                echo "Wrong. Please try a higher number!";
-                            } else {
-                                echo "Wrong. Please try a lower number!";
-                            }
-                        } else {
-                            echo "You must enter a number!";
-                        }
-                        ?>
-                    </td>
-                </tr>
-            <?php endif; ?>
-            <tr>
-                <td align="right">
-                    <input type="submit" value="Submit">
-                </td>
-                <td align="left">
-                    <input type="reset" value="Reset">
-                </td>
-            </tr>
-        </table>
+    
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+      <input type="text" name="result" value="<?php echo $result ?>" style="display: none;">
+      <input type="text" name="count" value="<?php echo $count ?>" style="display: none;">
+      <table>
+        <tr>
+          <td>Your guess:</td>
+          <td>
+            <input type="text" name="guess" value="<?php echo $guess; ?>">
+          </td>
+        </tr>
+        <?php if ($guess !== ''): ?>
+          <tr>
+            <td>
+              <?php
+                if (is_numeric($guess)) {
+                  if ($guess == $result) {
+                    echo "You are correct!";
+                  } elseif ($guess < $result) {
+                    echo "Wrong. Please try a higher number!";
+                  } else {
+                    echo "Wrong. Please try a lower number!";
+                  }
+                } else {
+                  echo "You must enter a number!";
+                }
+              ?>
+            </td>
+          </tr>
+        <?php endif; ?>
+        <tr>
+          <td>You have guessed:</td>
+          <td>
+            <?php echo $count, " times" ?>
+          </td>
+        </tr>
+        <tr>
+          <td align="right">
+            <input type="submit" value="Submit">
+          </td>
+          <td align="left">
+            <input type="reset" value="Reset">
+          </td>
+        </tr>
+      </table>
     </form>
-</body>
-
+  </body>
 </html>
